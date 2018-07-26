4d5b0932a5b8c99d7ed4169f35535ccca8dee0e9
Greta <greta.grigelionyte@gmail.com>
2018-07-26 15:51:16 +0300

    Update sWRegister.js

--- sWRegister.js
+++ sWRegister.js
@@ -1,6 +1,6 @@
 /*========= Service Worker Registration ============*/
 if(navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then(function(registrated) {
+  navigator.serviceWorker.register('./sw.js').then(function(registrated) {
     console.log('Service Worker successfuly registrated');
   }).catch(function (error) {
     console.log('Service Worker not registrated', error);
